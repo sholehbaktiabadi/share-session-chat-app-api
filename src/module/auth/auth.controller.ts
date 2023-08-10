5d4e9b6239f42c8c3bdd6146ac8de7ee2dbca2e0
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2023-08-10 12:43:59 +0700

    refc: auth module

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -2,15 +2,16 @@ import Router from '@koa/router'
 import { AuthService } from './auth.service'
 import { UserRepository } from '../user/user.repository'
 import { AppDataSource } from '../../config/mysql.config'
+import { success } from '../../helper/response'
 
 const dataSource = AppDataSource
 const userRepository = new UserRepository(dataSource)
 const authService = new AuthService(userRepository)
 const authRouter = new Router({ prefix: '/auth' })
 
-authRouter.post('/login', (ctx)=> {
+authRouter.post('/login', async (ctx)=> {
     const dto = ctx.request.body
-    return authService.login(ctx, dto)
+    return await authService.login(ctx, dto)
 })
 
 export default authRouter
